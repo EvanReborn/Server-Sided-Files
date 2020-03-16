39d5828ebbf2ce7063a2f9fad5652af649ff4412
Evan <56363914+Retransition@users.noreply.github.com>
2020-03-15 22:41:47 -0400

    Update index.php

--- Server-Side/index.php
+++ Server-Side/index.php
@@ -1,5 +1,5 @@
 <?php
-$con = mysqli_connect("localhost", "u792718613_Evan", "cLRN4dzVMK5qLhUJLJ", "u792718613_valance");
+$con = mysqli_connect("enter", "your", "own", "credentials");
 if (mysqli_connect_error()) {
     echo "Failed to connect to DB!";
 }
